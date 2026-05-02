3c474d28306bc3d9335f84fc60fc671141370f75
manachat72 <143947596+manachat72@users.noreply.github.com>
2026-05-02 17:12:00 +0700

    feat: implement Google Tag Manager for comprehensive site-wide tracking

--- components.js
+++ components.js
@@ -13,10 +13,12 @@ function _loadMarketingPixels() {
   _marketingPixelsLoaded = true;
 
   // Google Tag Manager
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-    j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-    f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T3F9WD5P');
+  if (!window.__btmusicdriveGtmLoaded) {
+    (function(w,d,s,l,i){w.__btmusicdriveGtmLoaded=true;w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T3F9WD5P');
+  }
 
   // Google Analytics 4
   var gaScript = document.createElement('script');
